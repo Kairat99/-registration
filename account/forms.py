67ff78f17afef778dbcc9d598cd0a29cf5403f76
Kairat <kairstos@gmail.com>
2019-12-08 23:03:48 +0600

    registration

---
+++ account/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from .models import Profile
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+    def register(request):
+        if request.method == 'POST':
+            user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в базе данных.
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+        else:
+            user_form = UserRegistrationForm()
+        return render(request, 'account/register.html', {'user_form': user_form})
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
